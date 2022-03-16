ed7d05039a465ed821254c62ad5515f973503049
Alhaque Khan <alhaquekhan@Alhaques-MacBook-Air.local>
2022-03-16 12:17:15 +0530

    Fourth Commit

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
-# Create your models here.
-class Lead(models.Model):
+class Lead(models.Model): 
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-   
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
